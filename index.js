5eef9c39b4b97b177ca95e7425cd0c548317a753
sagniKdas53 <paromita67das@gmail.com>
2020-12-22 00:09:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
+var d = new Date();
+var one = document.getElementById("9am");
+var two = document.getElementById("10am");
+var three = document.getElementById("12am");
+var four = document.getElementById("break");
+var five = document.getElementById("1pm");
+var six = document.getElementById("3pm");
+
+function timer() {
+  var t = d.getHours();
+  var m = d.getMinutes();
+  console.log(t + ":" + m);
+}
+
+setInterval(timer, 10000);
